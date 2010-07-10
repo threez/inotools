29a535e8ed9c3706d51c52edc1f32e0ce0d5c4d1
Vincent Landgraf <setcool@gmx.de>
2010-07-11 00:13:26 +0200

    added safari plugin to inject zep.js to the page

---
+++ zepinject.safariextension/zep.js
@@ -0,0 +1,3 @@
+script = document.createElement('script');
+script.setAttribute('src', 'http://github.com/threez/inotools/raw/master/zep.js');
+document.getElementsByTagName('body')[0].appendChild(script);
